0fcf2cd69f68443b7b6cccca2b1ff5cdb9065d68
reciprocum <reciprocum@users.noreply.github.com>
2016-09-06 15:07:35 +0100

    CamQ3_lookAtOriginUpwards tune-up
    Q_EPSILON was too small an alternative to use for zAxis colinear viewPoints. Using Q_1 >> 4 instead

--- src/c/CamQ3_lookAtOriginUpwards.c
+++ src/c/CamQ3_lookAtOriginUpwards.c
@@ -3,7 +3,7 @@
    File   : CamQ3_lookAtOriginUpwards.c
    Author : Afonso Santos, Portugal
 
-   Last revision: 14h07 August 29 2016
+   Last revision: 14h49 September 06 2016  GMT
 */
 
 #include "CamQ3.h"
@@ -19,12 +19,12 @@ CamQ3_lookAtOriginUpwards
 {
   // setup 3D camera
   return CamQ3_setup( cam
-                    , (viewPoint->x != Q_0  ||  viewPoint->y != Q_0)                // Viewpoint not on Z axis ?
-                      ? viewPoint                                                   // Use original view point.
-                      : &(Q3){ .x = Q_EPSILON, .y = Q_EPSILON, .z = viewPoint->z}   // Use epsilon displaced alternate view point (to avoid math indetermination).
-                    , &Q3_origin                                                    // Looking at.
-                    , &Q3_versorPlusZ                                               // Use Q3_versorPlusZ as vertical reference.
-                    , zoom                                                          // Zoom level.
-                    , projectionMode                                                // Projection mode.
+                    , (viewPoint->x != Q_0  ||  viewPoint->y != Q_0)           // Viewpoint not on Z axis ?
+                      ? viewPoint                                              // Use original view point.
+                      : &(Q3){ .x = Q_1>>4, .y = Q_1>>4, .z = viewPoint->z }   // Use nearby alternate view point (to avoid math indetermination).
+                    , &Q3_origin                                               // Looking at.
+                    , &Q3_versorPlusZ                                          // Use Q3_versorPlusZ as vertical reference.
+                    , zoom                                                     // Zoom level.
+                    , projectionMode                                           // Projection mode.
                     ) ;
 }
